6f23f521ec2824c1fe3ea5a0ed5fc66e057e734c
Ruben Rodrigo <ruben.chani@tecsup.edu.pe>
2021-05-03 23:17:08 -0500

    EsLint

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-import express from "express";
-import { persons } from "./persons.js";
+import express from 'express';
+import { persons } from './persons';
 
 const app = express();
 const port = 3000;
-const error = "Error 404. This page does not exist";
+const error = 'Error 404. This page does not exist';
 
 // Necesitamos esto para poder leer JSON
 app.use(express.json());
 
 // GET
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   const date = new Date();
   const info = `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`;
   res.send(info);
 });
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   res.send(persons);
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const person = persons.find(
-    (person) => person.id === parseInt(req.params.id)
+    (person) => person.id === parseInt(req.params.id, 10),
   );
   if (!person) {
     res.status(404).send(error);
@@ -34,17 +34,17 @@ app.get("/api/persons/:id", (req, res) => {
 });
 
 // POST
-app.post("/api/persons", (req, res) => {
+app.post('/api/persons', (req, res) => {
   const data = req.body;
   if (!data.name) {
-    res.status(400).send({ error: "name is required" });
+    res.status(400).send({ error: 'name is required' });
   } else if (!data.number) {
-    res.status(400).send({ error: "number is required" });
+    res.status(400).send({ error: 'number is required' });
   }
 
   const person = persons.find((person) => person.name === data.name);
   if (person) {
-    res.status(400).send({ error: "name must be unique" });
+    res.status(400).send({ error: 'name must be unique' });
   }
 
   const newPerson = {
@@ -57,9 +57,9 @@ app.post("/api/persons", (req, res) => {
 });
 
 // DELETE
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
   const person = persons.find(
-    (person) => person.id === parseInt(req.params.id)
+    (person) => person.id === parseInt(req.params.id, 10),
   );
   if (!person) {
     res.status(404).send(error);
@@ -69,10 +69,12 @@ app.delete("/api/persons/:id", (req, res) => {
   res.send(person);
 });
 
-app.get("*", (req, res) => {
-  res.status(404).send(error);
+// app.get("*", (req, res) => {
+// res.status(404).send(error);
+// });
+// Middleware
+app.use((req, res, next) => {
+  res.status(404).send('Sorry cant find that!');
 });
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
